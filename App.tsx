9954be4e4a2c926b81652c8bded43221fdf4314a
Ryan de Souza <ryan1858.rs@hotmail.com>
2023-09-12 23:02:37 -0300

    feat: Setups inicial projeto

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useCallback } from "react";
+import { Text, View } from "react-native";
 
 export default function App() {
+  const [fontsLoaded, fontsError] = useFonts({
+    "Montserrat-Regular": require("./src/assets/fonts/Montserrat-Regular.ttf"),
+    "Montserrat-Medium": require("./src/assets/fonts/Montserrat-Medium.ttf"),
+    "Montserrat-Bold": require("./src/assets/fonts/Montserrat-Bold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontsError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontsError]);
+
+  if (!fontsLoaded && !fontsError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      className="flex-1 items-center justify-center bg-slate-600"
+      onLayout={onLayoutRootView}
+    >
+      <Text className="font-text">
+        Open up App.tsx to start working on dsadsy onbghjvghfghfhur app!
+      </Text>
+      <StatusBar translucent />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
